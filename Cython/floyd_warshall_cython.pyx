8808e38748390f5a1974c27b8cfb7d27bdac851b
Abhijit Mondal <abhijit.mondal@walmart.com>
2019-01-02 22:08:44 +0530

    cython files

--- Cython/floyd_warshall_cython.pyx
+++
@@ -1,9 +0,0 @@
-def floydWarshall(double[:,:] dist_mat, int n):
-    cdef i, j, k
-    
-    for k in range(n): 
-        for i in range(n):
-            for j in range(n):
-                dist_mat[i,j] = min(dist_mat[i,j], dist_mat[i,k] + dist_mat[k,j]) 
-
-    return dist_mat
--- Cython/setup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name="floydWarshall", ext_modules=cythonize('floyd_warshall_cython.pyx'),)
