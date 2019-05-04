8435b0c43a89615fa4b6185379852ebb3ca6b4a5
Abhijit Mondal <abhi2iitk@gmail.com>
2019-05-04 15:07:37 +0530

    Update SelfCrossing.py

--- LeetCode/SelfCrossing.py
+++ LeetCode/SelfCrossing.py
@@ -1,73 +1,16 @@
+import collections
+
 class Solution(object):
     def isSelfCrossing(self, x):
-        curr_x, curr_y = 0, 0
-        h = []
+        if len(x) < 4:
+            return False
         
-        for i in range(len(x)):
-            a = (curr_x, curr_y)
+        for i in range(3, len(x)):
+            a = x[i] >= x[i-2] and x[i-1] <= x[i-3]
+            b = i >= 4 and x[i] + x[i-4] >= x[i-2] and x[i-2] > x[i-4] and x[i-1] == x[i-3]
+            c = i >= 5 and x[i] + x[i-4] >= x[i-2] and x[i-2] > x[i-4] and x[i-1] + x[i-5] >= x[i-3] and x[i-3] > x[i-5] and x[i-1] <= x[i-3]
             
-            if i % 2 == 0:
-                if i % 4 == 0:
-                    curr_y += x[i]
-                    j = 0
-                    while 0 <= j < len(h):
-                        p, q = min(h[j][0][0], h[j][1][0]), max(h[j][0][0], h[j][1][0])
-                        
-                        if a[1] < h[j][0][1] <= curr_y and p <= curr_x <= q:
-                            return True
-                        
-                        if h[j][0][1] > curr_y and h[j][1][1] > curr_y:
-                            h.pop(j)
-                            j -= 1
-                        j += 1
-                        
-                else:
-                    curr_y -= x[i]
-                    j = 0
-                    while 0 <= j < len(h):
-                        p, q = min(h[j][0][0], h[j][1][0]), max(h[j][0][0], h[j][1][0])
-                        
-                        if a[1] > h[j][0][1] >= curr_y and p <= curr_x <= q:
-                            return True
-                        
-                        if h[j][0][1] < curr_y and h[j][1][1] < curr_y:
-                            h.pop(j)
-                            j -= 1
-                        j += 1
-                    
-            else:
-                if (i+1) % 4 == 0:
-                    curr_x += x[i]
-                    j = 0
-                    while 0 <= j < len(h):
-                        p, q = min(h[j][0][1], h[j][1][1]), max(h[j][0][1], h[j][1][1])
-                        
-                        if a[0] < h[j][0][0] <= curr_x and p <= curr_y <= q:
-                            return True
-                        
-                        if h[j][0][0] > curr_x and h[j][1][0] > curr_x:
-                            h.pop(j)
-                            j -= 1
-                        j += 1
-                        
-                else:
-                    curr_x -= x[i]
-                    j = 0
-                    while 0 <= j < len(h):
-                        p, q = min(h[j][0][1], h[j][1][1]), max(h[j][0][1], h[j][1][1])
-                        
-                        if a[0] > h[j][0][0] >= curr_x and p <= curr_y <= q:
-                            return True
-                        
-                        if h[j][0][0] < curr_x and h[j][1][0] < curr_x:
-                            h.pop(j)
-                            j -= 1
-                        j += 1
-                    
-            if len(h) >= 4:
-                h.pop(0)
-                
-            b = (curr_x, curr_y)
-            h.append((a, b))
+            if a or b or c:
+                return True
             
         return False
