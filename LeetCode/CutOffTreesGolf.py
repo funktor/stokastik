f618c493c00d88d5a384f93eb59207c2f75797c0
Abhijit Mondal <abhi2iitk@gmail.com>
2019-05-05 11:43:35 +0530

    Update CutOffTreesGolf.py

--- LeetCode/CutOffTreesGolf.py
+++ LeetCode/CutOffTreesGolf.py
@@ -2,41 +2,44 @@ from collections import deque
 import heapq, numpy as np
 
 class Solution(object):
-    def add(self, forest, x, y, stack, stack_pos):
-        if 0 <= x < len(forest) and 0 <= y < len(forest[0]) and forest[x][y] != 0:
+    def add(self, forest, x, y, stack, visited):
+        if 0 <= x < len(forest) and 0 <= y < len(forest[0]) and forest[x][y] != 0 and (x, y) not in visited:
             stack.append((x, y))
-            stack_pos[(x, y)] = len(stack)-1
+            visited.add((x, y))
             return True
+        
         return False
     
-    
-    def get_add_status(self, forest, end, x, y, stack, stack_pos, p=1, q=1):
+    def get_add_status(self, forest, end, x, y, stack, visited, p=1, q=1, reverse=False):
         if end[0] == x:
-            z = self.add(forest, x, y+q, stack, stack_pos)
+            z = self.add(forest, x, y+q, stack, visited)
         elif end[1] == y:
-            z = self.add(forest, x+p, y, stack, stack_pos)
+            z = self.add(forest, x+p, y, stack, visited)
         else:
-            z = self.add(forest, x+p, y, stack, stack_pos) or self.add(forest, x, y+q, stack, stack_pos)
+            if reverse:
+                z = self.add(forest, x, y+q, stack, visited) or self.add(forest, x+p, y, stack, visited)
+            else:
+                z = self.add(forest, x+p, y, stack, visited) or self.add(forest, x, y+q, stack, visited)
         return z
     
     
-    def dfs_update(self, forest, stack, stack_pos, end):
+    def dfs_update(self, forest, stack, visited, end, reverse=False):
         curr_start = stack[-1]
         x, y = curr_start
 
         z = False
         
         if end[0] >= x and end[1] >= y:
-            z = self.get_add_status(forest, end, x, y, stack, stack_pos, 1, 1)
+            z = self.get_add_status(forest, end, x, y, stack, visited, 1, 1, reverse)
             
         elif end[0] >= x and end[1] <= y:
-            z = self.get_add_status(forest, end, x, y, stack, stack_pos, 1, -1)
+            z = self.get_add_status(forest, end, x, y, stack, visited, 1, -1, reverse)
             
         elif end[0] <= x and end[1] >= y:
-            z = self.get_add_status(forest, end, x, y, stack, stack_pos, -1, 1)
+            z = self.get_add_status(forest, end, x, y, stack, visited, -1, 1, reverse)
 
         else:
-            z = self.get_add_status(forest, end, x, y, stack, stack_pos, -1, -1)
+            z = self.get_add_status(forest, end, x, y, stack, visited, -1, -1, reverse)
 
         return z
             
@@ -44,25 +47,26 @@ class Solution(object):
     def dfs(self, forest, start, end):
         min_dist = float("Inf")
         
+        t_min_dist = abs(end[0]-start[0]) + abs(end[1]-start[1])
+        
         stack_fwd, stack_bwd = [start], [end]
-        stack_fwd_pos, stack_bwd_pos = {start:0}, {end:0}
+        visited_fwd, visited_bwd = set([start]), set([end])
         
         while True:
-            x = self.dfs_update(forest, stack_fwd, stack_fwd_pos, end)
+            x = self.dfs_update(forest, stack_fwd, visited_fwd, end)
+            y = self.dfs_update(forest, stack_bwd, visited_bwd, start, reverse=True)
             
             if x is False:
-                return -1
-            
-            if stack_fwd[-1] in stack_bwd_pos:
-                return stack_bwd_pos[stack_fwd[-1]] + len(stack_fwd) - 1
-                    
-            y = self.dfs_update(forest, stack_bwd, stack_bwd_pos, start)
-            
+                stack_fwd.pop()
+                
             if y is False:
+                stack_bwd.pop()
+            
+            if len(stack_fwd) == 0 or len(stack_bwd) == 0:
                 return -1
-                    
-            if stack_bwd[-1] in stack_fwd_pos:
-                return stack_fwd_pos[stack_bwd[-1]] + len(stack_bwd) - 1
+            
+            if stack_fwd[-1] in visited_bwd or stack_bwd[-1] in visited_fwd:
+                return t_min_dist
             
             
     def a_star_add(self, forest, end, x, y, dist, visited, min_heap, distances):
