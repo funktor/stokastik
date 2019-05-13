1e9c2b85634500c06fd3a8b98547a646f05335cc
Abhijit Mondal <abhi2iitk@gmail.com>
2019-05-13 19:30:33 +0530

    Update RandomConstant.py

--- LeetCode/RandomConstant.py
+++ LeetCode/RandomConstant.py
@@ -9,9 +9,9 @@ class Node(object):
 class RandomizedCollection(object):
 
     def __init__(self):
-        self.head, self.tail = None, None
-        self.obj_map, self.idx_map, self.idx_map2 = dict(), dict(), dict()
-        self.n = 0
+        self.tail = None
+        self.obj_map = dict()
+        self.node_list = []
 
     def insert(self, val):
         node = Node(val)
@@ -28,44 +28,35 @@ class RandomizedCollection(object):
         if out:
             self.obj_map[val] = set()
             
-        self.obj_map[val].add(self.n)
-        
-        self.idx_map[self.n] = node
-        self.idx_map2[node] = self.n
-        
-        self.n += 1
+        self.obj_map[val].add(node)
+        self.node_list.append(node)
         
         return out
         
 
     def remove(self, val):
         if val in self.obj_map:
-            idx = self.obj_map[val].pop()
-            
-            curr_node = self.idx_map[idx]
-            curr_node.val = self.tail.val
+            node = self.obj_map[val].pop()
+            node.val = self.tail.val
+            self.obj_map[node.val].add(node)
+            self.obj_map[node.val].remove(self.tail)
             
             self.tail = self.tail.prev
             
+            if self.tail is not None:
+                self.tail.next = None
+            
+            if len(self.node_list) > 0:
+                self.node_list.pop()
+                
             if len(self.obj_map[val]) == 0:
                 self.obj_map.pop(val)
-            
-            if curr_node.val in self.obj_map and self.n-1 in self.obj_map[curr_node.val]:
-                self.obj_map[curr_node.val].remove(self.n-1)
-                
-            if idx < self.n-1:
-                if curr_node.val not in self.obj_map:
-                    self.obj_map[curr_node.val] = set()
-                    
-                self.obj_map[curr_node.val].add(idx)
-                
-            self.n -= 1
             
             return True
         return False
         
 
     def getRandom(self):
-        if self.n > 0:
-            return self.idx_map[randint(0, self.n-1)].val
+        if len(self.node_list) > 0:
+            return random.choice(self.node_list).val
         return None
