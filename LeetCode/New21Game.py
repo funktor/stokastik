8f4d72485802872ddd2d6f0bf550d88ee4440753
Abhijit Mondal <abhi2iitk@gmail.com>
2019-07-23 21:10:58 +0530

    Update New21Game.py

--- LeetCode/New21Game.py
+++ LeetCode/New21Game.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def new21Game(self, N, K, W):
-        f, g = [0.0]*(N+1), [0.0]*(N+1)
+        f = [0.0]*(N+1)
+        last_sum = 0.0
         
         for k in range(N+1):
             if k <= N-K:
                 f[k] = 1.0
             else:
-                f[k] = (1.0/W)*g[k-1]
+                f[k] = (1.0/W)*last_sum
                 
-            g[k] = g[k-1] + f[k] - f[k-W] if k-W >= 0 else g[k-1] + f[k]
+            last_sum = last_sum + f[k] - f[k-W] if k-W >= 0 else last_sum + f[k]
                 
         return f[-1]
