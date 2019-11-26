d7679cd5c7635f8ca0f9d601464d92ec36e8fc7f
Abhijit Mondal <abhi2iitk@gmail.com>
2019-11-26 11:13:51 +0530

    Update matrix_mult_thread.cpp

--- CUDA/matrix_mult_thread.cpp
+++ CUDA/matrix_mult_thread.cpp
@@ -7,8 +7,6 @@
 #include <chrono>
 #include <thread>
 
-#define M_DIM 50
-
 std::vector<std::vector<int>> random_matrix(const int num_rows, const int num_cols, const int min_val=0.0, const int max_val=1000.0) {
     std::vector<std::vector<int>> my_arr;
     static std::random_device rd;
@@ -26,25 +24,25 @@ std::vector<std::vector<int>> random_matrix(const int num_rows, const int num_co
     return my_arr;
 }
 
-void multiply(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int p, const int start, const int end) {
+void multiply(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int m, const int p, const int start, const int end) {
     for (int index = start; index < end; index++) {
 	int row = index/p;
         int col = index % p;
 
 	int sum = 0;
-	for (int k = 0; k < M_DIM; k++) {
-	    sum += mat_1[row*M_DIM+k] * mat_2[k*p+col];
+	for (int k = 0; k < m; k++) {
+	    sum += mat_1[row*m+k] * mat_2[k*p+col];
 	}	
 	mat_prod[index] = sum;
     }
 }
 
-bool check_correctness(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int p) {
+bool check_correctness(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int m, const int p) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < p; j++) {
             int sum = 0;
-            for (int k = 0; k < M_DIM; k++) {
-                sum += mat_1[i*M_DIM+k] * mat_2[k*p+j];
+            for (int k = 0; k < m; k++) {
+                sum += mat_1[i*m+k] * mat_2[k*p+j];
             }
             if (sum != mat_prod[i*p+j]) {
                 return false;
@@ -55,27 +53,28 @@ bool check_correctness(const int *mat_1, const int *mat_2, int *mat_prod, const 
 }
 
 int main(void) {
-    int n = 5000;
-    int p = 8000;
+    int n = 2000;
+    int m = 1000;
+    int p = 5000;
     int n_threads = 100;
     
     int *mat_1, *mat_2, *mat_prod;
     std::vector<std::thread> threads(n_threads);
 
-    mat_1 = (int *)malloc(n*M_DIM*sizeof(int));
-    mat_2 = (int *)malloc(M_DIM*p*sizeof(int));
+    mat_1 = (int *)malloc(n*m*sizeof(int));
+    mat_2 = (int *)malloc(m*p*sizeof(int));
     mat_prod = (int *)malloc(n*p*sizeof(int));
     
-    std::vector<std::vector<int>> my_arr_1 = random_matrix(n, M_DIM, 0, 10);
-    std::vector<std::vector<int>> my_arr_2 = random_matrix(M_DIM, p, 0, 10);
+    std::vector<std::vector<int>> my_arr_1 = random_matrix(n, m, 0, 10);
+    std::vector<std::vector<int>> my_arr_2 = random_matrix(m, p, 0, 10);
     
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < M_DIM; j++) {
-            mat_1[M_DIM*i + j] = my_arr_1[i][j];
+        for (int j = 0; j < m; j++) {
+            mat_1[m*i + j] = my_arr_1[i][j];
         }
     }
     
-    for (int i = 0; i < M_DIM; i++) {
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < p; j++) {
             mat_2[p*i + j] = my_arr_2[i][j];
         }
@@ -87,7 +86,7 @@ int main(void) {
     for (int i = 0; i < n_threads; i++) {
         int start = k*i;
         int end = std::min(k*(i+1), n*p);
-        threads[i] = std::thread(multiply, mat_1, mat_2, mat_prod, n, p, start, end);
+        threads[i] = std::thread(multiply, mat_1, mat_2, mat_prod, n, m, p, start, end);
     }
 
     for (int i = 0; i < n_threads; i++) {
@@ -98,8 +97,11 @@ int main(void) {
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
 
     std::cout << duration << std::endl;
-    std::cout << check_correctness(mat_1, mat_2, mat_prod, n, p) << std::endl;
+    std::cout << check_correctness(mat_1, mat_2, mat_prod, n, m, p) << std::endl;
+    
+    free(mat_1);
+    free(mat_2);
+    free(mat_prod);
 
     return 0;
 }
-
