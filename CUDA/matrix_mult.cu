48a02fbd8fa27bb141a6d4a88c18bd2b96c886d9
Abhijit Mondal <abhi2iitk@gmail.com>
2019-11-26 10:59:27 +0530

    Update matrix_mult.cu

--- CUDA/matrix_mult.cu
+++ CUDA/matrix_mult.cu
@@ -7,57 +7,43 @@
 #include <chrono>
 
 #define TILE_DIM 32
-#define M_DIM 50
 
-__global__ void multiplyNaive(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int p) {
+__global__ void multiplyNaive(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int m, const int p) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (y < n && x < p) {
         int sum = 0;
     
-        for (int i = 0; i < M_DIM; i++) {
-            sum += mat_1[y*M_DIM+i] * mat_2[i*p+x];
+        for (int i = 0; i < m; i++) {
+            sum += mat_1[y*m+i] * mat_2[i*p+x];
         }
     
         mat_prod[y*p+x] = sum;
     }
 }
 
-__global__ void multiplySharedMem(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int p) {
-    __shared__ float aTile[TILE_DIM][M_DIM], bTile[M_DIM][TILE_DIM];
+__global__ void multiplySharedMem(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int m, const int p, const int start, const int end) {
+    __shared__ int aTile[TILE_DIM][TILE_DIM], bTile[TILE_DIM][TILE_DIM];
 
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     
-    int h = (M_DIM + blockDim.x - 1)/blockDim.x;
-    
-    int start_a = h*threadIdx.x;
-    int end_a = M_DIM < h*(threadIdx.x+1)?M_DIM:h*(threadIdx.x+1);
-    
-    for (int i = start_a; i < end_a; i++) {
-        aTile[threadIdx.y][i] = mat_1[y*M_DIM+i];
-    }
-    
-    h = (M_DIM + blockDim.y - 1)/blockDim.y;
-    
-    int start_b = h*threadIdx.y;
-    int end_b = M_DIM < h*(threadIdx.y+1)?M_DIM:h*(threadIdx.y+1);
-    
-    for (int i = start_b; i < end_b; i++) {
-        bTile[i][threadIdx.x] = mat_2[i*p+x];
-    }
+    aTile[threadIdx.y][threadIdx.x] = mat_1[y*m+threadIdx.x+start];
+    bTile[threadIdx.y][threadIdx.x] = mat_2[(threadIdx.y+start)*p+x];
     
     __syncthreads();
+    
+    int dims_tile = end-start;
     
     if (y < n && x < p) {
         int sum = 0;
     
-        for (int i = 0; i < M_DIM; i++) {
+        for (int i = 0; i < dims_tile; i++) {
             sum += aTile[threadIdx.y][i] * bTile[i][threadIdx.x];
         }
     
-        mat_prod[y*p+x] = sum;
+        mat_prod[y*p+x] += sum;
     }
 }
 
@@ -78,12 +64,12 @@ std::vector<std::vector<int>> random_matrix(const int num_rows, const int num_co
     return my_arr;
 }
 
-bool check_correctness(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int p) {
+bool check_correctness(const int *mat_1, const int *mat_2, int *mat_prod, const int n, const int m, const int p) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < p; j++) {
             int sum = 0;
-            for (int k = 0; k < M_DIM; k++) {
-                sum += mat_1[i*M_DIM+k] * mat_2[k*p+j];
+            for (int k = 0; k < m; k++) {
+                sum += mat_1[i*m+k] * mat_2[k*p+j];
             }
             if (sum != mat_prod[i*p+j]) {
                 return false;
@@ -94,42 +80,59 @@ bool check_correctness(const int *mat_1, const int *mat_2, int *mat_prod, const 
 }
 
 int main(void) {
-    int n = 5000;
-    int p = 8000;
+    int n = 2000;
+    int m = 1000;
+    int p = 5000;
 
     dim3 dimGrid((p + TILE_DIM - 1)/TILE_DIM, (n + TILE_DIM - 1)/TILE_DIM, 1);
     dim3 dimBlock(TILE_DIM, TILE_DIM, 1);
     
-    int *mat_1, *mat_2, *mat_prod; 
+    int *mat_1, *mat_2, *mat_prod;
 
-    cudaMallocManaged(&mat_1, n*M_DIM*sizeof(int));
-    cudaMallocManaged(&mat_2, M_DIM*p*sizeof(int));
+    cudaMallocManaged(&mat_1, n*m*sizeof(int));
+    cudaMallocManaged(&mat_2, m*p*sizeof(int));
     cudaMallocManaged(&mat_prod, n*p*sizeof(int));
     
-    std::vector<std::vector<int>> my_arr_1 = random_matrix(n, M_DIM, 0, 10);
-    std::vector<std::vector<int>> my_arr_2 = random_matrix(M_DIM, p, 0, 10);
+    std::vector<std::vector<int>> my_arr_1 = random_matrix(n, m, 0, 10);
+    std::vector<std::vector<int>> my_arr_2 = random_matrix(m, p, 0, 10);
     
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < M_DIM; j++) {
-            mat_1[M_DIM*i + j] = my_arr_1[i][j];
+        for (int j = 0; j < m; j++) {
+            mat_1[m*i + j] = my_arr_1[i][j];
         }
     }
     
-    for (int i = 0; i < M_DIM; i++) {
+    for (int i = 0; i < m; i++) {
         for (int j = 0; j < p; j++) {
             mat_2[p*i + j] = my_arr_2[i][j];
         }
     }
     
+    int num_parts = (m+TILE_DIM-1)/TILE_DIM;
+    cudaStream_t stream[num_parts];
+
     auto t1 = std::chrono::high_resolution_clock::now();
-    multiplySharedMem<<<dimGrid, dimBlock>>>(mat_1, mat_2, mat_prod, n, p);
+    
+    for (int i = 0; i < num_parts; i++) {
+        int start = i*TILE_DIM;
+        int end = m < ((i+1)*TILE_DIM)?m:((i+1)*TILE_DIM);
+        
+        cudaStreamCreate(&stream[i]);
+        multiplySharedMem<<<dimGrid, dimBlock, 0, stream[i]>>>(mat_1, mat_2, mat_prod, n, m, p, start, end);
+    }
+    
     cudaDeviceSynchronize();
+
     auto t2 = std::chrono::high_resolution_clock::now();
     
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
 
     std::cout << duration << std::endl;
-    std::cout << check_correctness(mat_1, mat_2, mat_prod, n, p) << std::endl;
+    std::cout << check_correctness(mat_1, mat_2, mat_prod, n, m, p) << std::endl;
+    
+    for (int i = 0; i < num_parts; i++) {
+        cudaStreamDestroy(stream[i]);
+    }
     
     cudaFree(mat_1);
     cudaFree(mat_2);
@@ -137,4 +140,3 @@ int main(void) {
     
     return 0;
 }
-
